c5ef7e525bc600c960d187512f246e602edf3b51
Tyson Nguyen <AKATYSON_XD@hotmail.com>
2022-12-02 11:30:49 +1100

    logo added and staffpage

---
+++ ROIStaffApp/ViewModels/CreatePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using ROIStaffApp.Models;
+using ROIStaffApp.Services;
+
+namespace ROIStaffApp.ViewModels
+{
+    internal class CreatePageViewModel : ViewModelBase
+    {
+        public CreatePageViewModel()
+        {
+
+        }
+    }
+}
